cad1e0e332870edcdb8b2c510bd6c8c40b9e8ab3
shihomi.tsuji <shihomi13019@gmail.com>
2021-01-29 08:49:06 +0900

    テキスト完了-Home画面で１投稿分しか表示できない

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 辻志保美 on 2021/01/23.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
